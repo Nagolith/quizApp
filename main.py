353c4a1070b0c9a4af63902a2288eb8355956f17
Jonathan Hald <jonathanmhald@gmail.com>
2022-09-19 14:56:59 -0600

    Added dark and light mode

--- main.py
+++ main.py
@@ -10,34 +10,44 @@ root.title("Welcome to Quiz making App")
 # Set geometry(width x height)
 root.geometry('1000x800')
 
+# function to iterate text when
+# button is clicked
+def clicked():
+    iterations = int(txt.get())
+    for i in range (iterations):
+        num = i + 1
+        lbl = Label(root, text = "Question number " + str(num))
+        lbl.grid()
+
+# set background to dark mode
+def dark_mode():
+    root['bg'] = 'black'
+
+# set background to light mode
+def light_mode():
+    root['bg'] = 'white'
+
+
 # adding menu bar in root window
 # new item in menu bar labelled as 'New'
 # adding more items in the menu bar
-menu = Menu(root)
-item = Menu(menu)
-item.add_command(label='New')
-item.add_command(label='Close')
-menu.add_cascade(label='File', menu=item)
-root.config(menu=menu)
+menubar = Menu(root)
+item = Menu(menubar, tearoff = 0)
+menubar.add_cascade(label='File', menu = item)
+item.add_command(label='New', command = None)
+item.add_command(label='Light mode', command = light_mode)
+item.add_command(label='Dark mode', command = dark_mode)
+item.add_separator()
+item.add_command(label='Close', command = root.destroy)
+
 
 # adding a label to the root window
 lbl = Label(root, text = "How many quiz questions would you like to make?")
 lbl.grid()
 
 # adding Entry Field
-txt = Entry(root, width=10)
-txt.grid(column=1, row=0)
-
-# function to display text when
-# button is clicked
-def clicked():
-    #res = "You wrote " + txt.get()
-    #lbl.configure(text = res)
-    iterations = int(txt.get())
-    for i in range (iterations):
-        lbl = Label(root, text = "Question number ")
-        lbl.grid()
-
+txt = Entry(root, width = 10)
+txt.grid(column = 1, row = 0)
 
 # button widget with red color text
 # inside
@@ -46,11 +56,9 @@ btn = Button(root, text = " Generate lines" ,
 
 
 
-# iterating over quiz questions
-
-
 # set Button grid
 btn.grid(column=2, row=0)
 
 # Execute Tkinter
+root.config(menu=menubar)
 root.mainloop()

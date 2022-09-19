77202836991f7ce3617cd7d02dbbbf0ba6086e5e
Jonathan Hald <jonathanmhald@gmail.com>
2022-09-18 18:49:55 -0600

    adding to the frame

--- main.py
+++ main.py
@@ -5,19 +5,34 @@ from tkinter import *
 root = Tk()
 
 # root window title and dimension
-root.title("Welcome to GeekForGeeks")
+root.title("Welcome to Quiz making App")
 
 # Set geometry(width x height)
-root.geometry('350x200')
+root.geometry('1000x800')
+
+# adding menu bar in root window
+# new item in menu bar labelled as 'New'
+# adding more items in the menu bar
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+item.add_command(label='Close')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
 
 # adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
+lbl = Label(root, text = "Ready to make a quiz?")
 lbl.grid()
+
+# adding Entry Field
+txt = Entry(root, width=10)
+txt.grid(column=1, row=0)
 
 # function to display text when
 # button is clicked
 def clicked():
-    lbl.configure(text = "I just got clicked")
+    res = "You wrote " + txt.get()
+    lbl.configure(text = res)
 
 # button widget with red color text
 # inside
@@ -25,7 +40,7 @@ btn = Button(root, text = " Click me" ,
              fg = "red", command = clicked)
 
 # set Button grid
-btn.grid(column=1, row=0)
+btn.grid(column=2, row=0)
 
 # Execute Tkinter
 root.mainloop()
